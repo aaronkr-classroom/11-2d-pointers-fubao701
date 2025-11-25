59e42f3d171db5539498fa6b7197136fa47392d9
ththfql <145094239+ththfql@users.noreply.github.com>
2025-11-25 16:57:25 +0900

    finish

---
+++ Project1/2d_ptr.c
@@ -0,0 +1,12 @@
+﻿// 2d_ptr.c
+#include <stdio.h>
+
+int main(void) {
+	short data = 3,
+		* p = &data, // data 변수의 주소
+		** pp = &p;  // 포인터 p의 주소 (2차원)
+
+	printf("[Before  ] data: %d\n", data); //3
+	*p = 40;
+	printf("[Use up  ] data: %d\n")
+}
